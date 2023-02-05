c52ea821d81ee11ec0ae114ff7a253814e2cc153
Azher Uddin Ahmed <azher305uddin@gmail.com>
2023-02-05 23:38:41 +0600

    feat: card page api call

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,5 +1,26 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { getTokenCookie } from "../../utils/utils"
 
 export default function CartPage () {
+
+    useEffect(() => {
+        axios.get('https://yurr.app/api/user/card', {
+            headers: {
+                Authorization: `Bearer ${getTokenCookie()}`
+            }
+        }).then(res => {
+            console.log(res.data)
+        });
+        
+        axios.get('https://yurr.app/api/user/favourite', {
+            headers: {
+                Authorization: `Bearer ${getTokenCookie()}`
+            }
+        }).then(res => {
+            console.log(res.data)
+        });
+    }, [])
 
     return (
         <div>
